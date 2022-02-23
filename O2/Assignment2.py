09ed0676ffa397196c5fde88d5bdc6f17e5287d3
Sander Aakerholt <sander.aa96@hotmail.com>
2022-02-23 16:31:32 +0100

    Tested how i could show plots in WSL, and got it to work

--- O2/Assignment2.py
+++ O2/Assignment2.py
@@ -1,16 +1,16 @@
 import numpy as np
 import glob
 from PIL import Image
-#import cv2
 import matplotlib.pyplot as plt
 ## Task 4: Prediction
 
 img_path = glob.glob('Program_Files/*.png')
+print(img_path)
 img_data = []
 for path in img_path:
     img_data.append(np.array(Image.open(path)))
 
+
 print(img_data[0])
 plt.figure()
 plt.imshow(img_data[0])
-#cv2.imshow(("Window", img_data[0]))
