f6f57ae33bcb61b96d157e0dcd62c242e7a78b63
SakariaHAli <46136295+SakariaHAli@users.noreply.github.com>
2022-04-22 13:02:05 +0200

    Add function for uniformquantizing. Works for positive values.

--- O3/problem1d_g.py
+++ O3/problem1d_g.py
@@ -1,3 +1,4 @@
+from random import sample
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -24,7 +25,37 @@ sigmasq = 1.0
 D = np.linspace(1e-6, 2*sigmasq,10000)
 R = np.maximum(0.5*np.log(sigmasq/D),0)
 
-plt.figure()
-plt.plot(D, R)
+# plt.figure()
+# plt.plot(D, R)
 
-plt.show()
+# plt.show()
+
+## 1e)
+def uniform_quantizer(samples,nbits):
+    quantizedSample = []
+    sorted_samples = np.sort(samples)
+    minVal = sorted_samples[0]
+    maxVal = sorted_samples[-1]
+    ValRange = maxVal - minVal
+    quantInterval = ValRange/(2**nbits)
+
+    for sample in samples:
+        curr_interval = quantInterval + quantInterval/2
+        while(sample>curr_interval):
+            curr_interval += quantInterval
+        quantizedSample.append(curr_interval-quantInterval/2)
+
+    return quantizedSample
+
+print("_____quantized_____\n")
+quantized_samples = np.sort(uniform_quantizer(norm_val,3))
+sorted_samples = np.sort(norm_val)
+
+for x in range(0,100):
+    print(quantized_samples[x],sorted_samples[x])
+
+
+
+##def quantize(nbits):
+  ##  for()
+    
